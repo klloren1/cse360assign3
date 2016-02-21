4201a278af57f5ce774e0d4b9fbf61df1a224098
Kyle Lorenzano <kylelorenzano@hotmail.com>
2016-02-21 13:12:43 -0700

    Initial Version

---
+++ cse360_assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,74 @@
+
+package cse360assign3;
+
+/**
+ * Class for performing mathematical operations via a simulated calculator.
+ * 
+ * @author Kyle Lorenzano PIN:517
+ *
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constructs a Calculator object with total initialized to zero.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the running total after operations.
+	 * 
+	 * @return integer of current total.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds a given integer to the total.
+	 * 
+	 * @param value integer to be added.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts a given integer from the total.
+	 * 
+	 * @param value integer to be subtracted.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies the total by a given integer.
+	 * 
+	 * @param value integer to multiply by.
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides the total by a given integer.
+	 * 
+	 * @param value integer to divide by.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns the history of operations in a string.
+	 * 
+	 * @return String of operational history.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
